0d34b28996ddaf19a340721f01e63d04a8bd5d97
artwired <bradcoledesign@gmail.com>
2014-12-17 09:14:57 -0800

    update to styles and addition of dynamic hex indicator

--- js/etch-sketch.js
+++ js/etch-sketch.js
@@ -17,16 +17,16 @@ $(document).ready(function() {
 	$("#resetGrid").on("click", function() {
 
 		$("#container div").remove();
-		// var div = "";
+		$("#initialText").remove();
 		var specNum = prompt("How many squares per side to make the new grid?");
 		if (specNum > 100) {
 			prompt("Sorry, that will take way too long. Please pick a number between 1 and 100");
 		};
 		var promptDiv = "";
-		var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
 			var specNumDiv = specNum;
 			var specNumTotal = specNum*specNum + 1;
 			var dynamicDivScale = containerSize / specNumDiv;
+			var oddNum = [7, 11, 17, 19, 26, 28, 33, 34, 35, 46, 47, 49, 51, 54, 55, 56, 57, 58, 62, 64, 65, 69, 76, 77, 78, 79, 83, 84, 85, 86, 87, 91, 94, 95, 99];
 			for (var i = 0; i < specNumTotal; i++) {
 				promptDiv += "<div class='reg'></div>";
 				if (i%specNum == 0) {
@@ -35,9 +35,12 @@ $(document).ready(function() {
 			}
 			$("body #container").append(promptDiv);
 			$("#container div.reg").css({"width" : dynamicDivScale, "height" : dynamicDivScale});
+			var randomColor = Math.floor(Math.random()*16777215).toString(16);
+			var backgroundColor = "background-color";
+			$("#colorcode").text("Pen Hex Color Value Is: #" + randomColor);
 			$('#container').find('div').first().remove();
 			$("#container div").hover(function() {
-			$(this).css("background-color", hue);
-	});
+			$(this).css({backgroundColor: '#' + randomColor});
+		});
 	});
 });
